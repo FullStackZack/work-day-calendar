d6842af8cf127eded3dad9ba25fb8805616be7cd
Zack White <zhwhite18@yahoo.com>
2020-08-08 14:14:08 -0500

    second commit

---
+++ script.js
@@ -0,0 +1,14 @@
+$("#currentDay").text(moment().format("dddd, MMMM Do"))
+
+var militaryHours = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+var currentHour = moment().hour()
+
+for (i = 0; i < militaryHours.length; i++) {
+    if (currentHour === militaryHours[i]) {
+        $("#" + i).addClass("present")
+    } else if (currentHour > militaryHours[i]) {
+        $("#" + i).addClass("past")
+    } else if (currentHour < militaryHours[i]) {
+        $("#" + i).addClass("future") 
+    }
+}
